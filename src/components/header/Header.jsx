8af2edda290fe51cca4ef1fe1676b877d69ec8e8
jade0819 <break0819@gmail.com>
2023-09-13 10:49:27 +0900

    Feat: 필터 버튼 토글 기능 추가

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="fixed left-0 top-0 w-full h-20 flex justify-center items-center bg-orange-300">
+    <div className="fixed left-0 top-0 w-full h-20 flex justify-center items-center bg-orange-pastel">
       <h2 className="text-2xl font-semibold text-white">시계열 차트</h2>
     </div>
   );
--- src/constants/colors.js
+++ src/constants/colors.js
@@ -5,9 +5,8 @@ export const chartColors = {
 
 export const colors = {
   skyblue: 'rgba(181, 217, 241, 0.6)',
-  skyblueAccent: 'rgba(159, 205, 231, 1)',
-
+  skyblueAccent: 'rgba(159, 205, 231, 0.6)',
   pink: 'rgba(248, 188, 214, 0.6)',
-  pinkAccent: 'rgba(255, 100, 147, 0.8)',
+  pinkAccent: 'rgba(255, 100, 147, 0.6)',
   whiteCloudy: 'rgba(240, 240, 240, 0.5)',
 };
